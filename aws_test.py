5e0ffe75dca88071ded0aa9e7d4d978199b6b5bd
ramkisoa <ramki.soatesting@yahoo.com>
2023-04-04 08:48:55 -0400

    initial commit

---
+++ aws_test.py
@@ -0,0 +1,12 @@
+
+import boto3
+
+
+
+s3_resource = boto3.resource('s3')
+
+test = s3_resource.Object(bucket_name='ramatestbucket04', key='sample.txt').get()['Body'].read().decode('utf-8').splitlines()
+
+print(test)
+
+
