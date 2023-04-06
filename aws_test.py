cd72930135cba9f787317e73a41ea670099f1493
Rama <ramki.soatesting@yahoo.com>
2023-04-06 17:58:36 -0400

    test commit12

--- aws_test.py
+++ aws_test.py
@@ -1,12 +1,8 @@
-
 import boto3
-
-
+import pyspark
 
 s3_resource = boto3.resource('s3')
 
 test = s3_resource.Object(bucket_name='ramatestbucket04', key='sample.txt').get()['Body'].read().decode('utf-8').splitlines()
 
 print(test)
-
-# This  test
